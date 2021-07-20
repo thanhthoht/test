5ebfcf9070ea30e4b38e7cce5652ac82c8313de5
leanht1k59 <thanhthoht@gmail.com>
2021-07-20 22:06:51 +0700

    22-06

--- test.cpp
+++ test.cpp
@@ -1,6 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    cout << "Duoc roi";
+struct node{
+    int data;
+    node  *left;
+    node *right;
+};
+node *creat( int x){
+    node *temp = new node();
+    temp->data = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+void insert(node **hientai, node *h){
+    if ((*hientai) == NULL){
+        (*hientai) = h;
+        return;
+    }
+    if ((*hientai)->data < h->data){
+        insert(&(*hientai)->left, h);
+    }else{
+        insert(&(*hientai)->right, h);
+    }
+}
+void printft(node *root){
+    if (root != NULL){
+        printft(root->right);
+        cout << root->data <<" ";
+        printft(root->left);
+    }
+}
+int main(){
+    int n, x;
+    cin >> n >> x;
+    node *root = creat(x);
+   
+    
+    node *temp = new node();
+    for (int i = 1; i < n; i++){
+        cin >> x;
+        temp = creat(x);
+        insert(&root , temp);
+    }
+    printft (root);
+    return 0;
 }
